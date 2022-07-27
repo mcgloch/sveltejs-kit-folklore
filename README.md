7b069e7bca1c4299a0fc906cfe27ab0359815ece
mcgloch <leanne_mcl@hotmail.com>
2022-07-27 09:54:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltejs-kit-template-default-z5qmp4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-z5qmp4)
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<script>
+import HeroSection from '$lib/HeroSection.svelte';
+</script>
+
+<main>
+<HeroSection />
+</main>
